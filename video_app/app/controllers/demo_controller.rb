f8f4370ed20216ed58aad35319d013ec576e48cf
webonise <webonise@webonise-desktop.(none)>
2012-10-17 19:05:03 +0530

    after url parameters video

--- video_app/app/controllers/demo_controller.rb
+++ video_app/app/controllers/demo_controller.rb
@@ -1,14 +1,16 @@
 class DemoController < ApplicationController
   def hello
     @array=[1,2,3,4,5]
+    @id=params[:id]
+    @page=params[:page].to_i
   end
 
   def index
-
+   #render ('demo/hello')
   end
 
   def other_hello
-    render{}
+
   end
 
 end
